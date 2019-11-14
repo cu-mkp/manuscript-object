8ec10805582065855046215c2b46b3abb1b96cbb
matthewkumar <matthewravikumar@gmail.com>
2019-11-14 14:01:19 -0500

    updated travis and update

--- update.py
+++ update.py
@@ -1,17 +1,39 @@
-# Last Updated | 2019-11-10
+# Last Updated | 2019-11-14
 
-import os
 from datetime import datetime
+from typing import List
+
+def update_metadata():
+  pass
+
+def update_ms_txt()
+  pass
+
+def update_all()
+  pass
+
+def update_time():
+  """ Extract timestamp at the top of this file and update it. """
+  # Initialize date to write and container for the text
+  now_str = str(datetime.now()).split(' ')[0]
+  lines = []
+
+  # open file, extract text, and modify
+  with open('./update.py', 'r') as f:
+    lines = f.read().split('\n')
+    lines[0] = "# Last Updated | " + now_str
+  
+  # write modified text
+  f = open('./update.py', 'w')
+  f.write('\n'.join(lines))
+  f.close
 
 def update():
-  now = datetime.strptime(str(datetime.now()).split(' ')[0], '%Y-%m-%d')
-  with open('./update.py', 'r') as f:
-    text = f.read()
-    comment = text.split('\n')[0]
-    timestamp = datetime.strptime(comment.split('|')[1].strip(), '%Y-%m-%d')
-
-    if timestamp < now:
-      print(f'The repository has not been updated since {timestamp}. Please run update.py on the day of merging the branch.')
-      assert False
+  """ Run each update function. """
+  update_metadata()
+  update_ms_txt()
+  update_all()
+  update_time()
+  print('updated!')
 
 update()
