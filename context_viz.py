903d7e9611f5e227e6f49abd46ca9a3d91c21d98
ronikaufman <ronikaufman97@gmail.com>
2020-05-27 15:56:35 +0200

    data transformation to generate viz

--- context_viz.py
+++ context_viz.py
@@ -3,6 +3,8 @@ import csv
 import sys
 import os
 
+import pandas
+
 cwd = os.getcwd()
 m_path = cwd if 'manuscript-object' not in cwd else f'{cwd}/../'
 m_k_data_to_context = f'{m_path}/manuscript-object/context'
@@ -13,9 +15,15 @@ if not os.path.exists(m_k_data_to_context):
 tags = ["al", "bp", "cn", "env", "m", "md", "ms", "mu", "pa", "pl", "pn", "pro", "sn", "tl", "tmp", "wp"] # which tags we're looking for
 manuscript_version = "tl" # "tl", "tc" or "tcn"
 
-def filter_stropwords(word):
+def filter_stopwords(word):
     stopwords = ["ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than"]
     if word in stopwords:
+        return False
+    else:
+        return True
+
+def filter_digits(word):
+    if word.isdigit():
         return False
     else:
         return True
@@ -31,14 +39,38 @@ for tag in tags:
         line_count = 0
         for row in csv_reader:
             if line_count > 0:
-                str = row[2] + row[3]
+                str = (row[2] + row[3]).lower()
                 remove_chars = ["[", "]", "'", ",", '&']
                 for char in remove_chars:
                     str = str.replace(char, "")
                 word_list = str.split()
-                word_list = filter(filter_stropwords, word_list)
+                # remove stopwords
+                word_list = filter(filter_stopwords, word_list)
+                # remove digits
+                word_list = filter(filter_digits, word_list)
+                # remove duplicates
+                word_list = list(dict.fromkeys(word_list))
                 if word_list != []:
                     this_context += word_list
             line_count += 1
 
     all_context.append(this_context)
+
+# heatmap visualization
+# how similar are contexts from different tags
+matrix = []
+for i in range(len(tags)):
+    l = []
+    for j in range(len(tags)):
+        l.append(0)
+    matrix.append(l)
+
+matrix = []
+for i in all_context:
+    si = set(i)
+    line = []
+    for j in all_context:
+        sj = set(j)
+        line.append(len(si.intersection(sj))/len(si.union(sj))*100)
+    matrix.append(line)
+df = pandas.DataFrame(matrix, index = tags, columns = tags)
