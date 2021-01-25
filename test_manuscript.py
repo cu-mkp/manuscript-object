4f74bc7815622123ba98c57e539a8519d9663982
gschare <grschare1315@gmail.com>
2021-01-25 12:58:01 -0800

    test manuscript skeleton code

---
+++ test_manuscript.py
@@ -0,0 +1,82 @@
+import unittest
+import utils
+from manuscript import *
+
+class ManuscriptEntry(unittest.TestCase):
+    def test_ignore_data_path(self):
+        pass
+
+    def test_extract_folio(self):
+        pass
+
+    def test_filename_from_folio(self):
+        pass
+
+    def test_clean_folio(self):
+        pass
+
+    def test_clean_id(self):
+        pass
+
+    def test_separate_by_id(self):
+        pass
+
+    def test_generate_entries(self):
+        pass
+
+    def test_generate_folios(self):
+        pass
+
+    def test_init(self):
+        pass
+
+    def test_get_entry(self):
+        pass
+
+    def test_get_folio(self):
+        pass
+
+    def test_add_entry(self):
+        pass
+
+    def test_add_entries(self):
+        pass
+
+    def test_add_folio(self):
+        pass
+
+    def test_add_dir(self):
+        pass
+
+    def test_add_dirs(self):
+        pass
+
+    def test_from_dir(self):
+        pass
+
+    def test_from_dirs(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_update_ms_txt(self):
+        pass
+
+    def test_update_entries(self):
+        pass
+
+    def test_update_all_folios(self):
+        pass
+
+    def test_generate_all_folios(self):
+        pass
+
+    def test_update_metadata(self):
+        pass
+
+    def test_generate_metadata(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
