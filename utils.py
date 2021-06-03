d155de969575c35faa33518d01ea5c0be0be1b63
gschare <grschare1315@gmail.com>
2021-06-03 15:05:10 -0400

    Ensure absolute paths for utils

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 
-manuscript_data_path = os.path.join(os.path.dirname(os.getcwd()), "m-k-manuscript-data") # default m&k data directory
+manuscript_data_path = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), "m-k-manuscript-data") # default m&k data directory
 ms_xml_path = os.path.join(manuscript_data_path, "ms-xml")
 versions = ['tc', 'tcn', 'tl']
 version_paths = [os.path.join(ms_xml_path, version) for version in versions]
@@ -67,4 +67,4 @@ categories = [
     "manuscript structure" 
 ]
 
-stylesheet_path = "annotations.xslt"
+stylesheet_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "annotations.xslt")
